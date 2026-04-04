e6f6609d6d1d78df3f35585bd3ac222cb41942c1
Bereczki Sándor Máté <bsandor453@gmail.com>
2026-04-04 19:15:53 +0200

    Create the Apollo Client and add it as a provider to the root

---
+++ src/api/apollo-client.ts
@@ -0,0 +1,17 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+
+const cache = new InMemoryCache();
+
+const link = new HttpLink({
+  // Zoosh TMDB sandbox URI
+  uri: 'https://tmdb.sandbox.zoosh.ie/',
+});
+
+export const apolloClient = new ApolloClient({
+  cache: cache,
+  link: link,
+  clientAwareness: {
+    name: 'movie-finder-web-proto',
+    version: '1.0',
+  },
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { ApolloProvider } from '@apollo/client/react';
 import App from './App.tsx';
+import { apolloClient } from './api/apollo-client.ts';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
     </StrictMode>,
   );
 } else {
