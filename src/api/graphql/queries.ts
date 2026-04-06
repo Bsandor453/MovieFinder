345a67e1c37c2725532dac78aaf6639e23a00f81
Bereczki Sándor Máté <bsandor453@gmail.com>
2026-04-06 18:56:39 +0200

    Add a query for similar movies and create a movie fragment for reusability

--- src/api/graphql/queries.ts
+++ src/api/graphql/queries.ts
@@ -1,19 +1,47 @@
 import { gql } from '@apollo/client';
 
+/**
+ * Reusable movie fragment
+ */
+export const MOVIE_FIELDS = gql`
+    fragment MovieFields on Movie {
+        id
+        name
+        overview
+        score
+        releaseDate
+        genres {
+            name
+        }
+        img: poster {
+            url: custom(size: "w500")
+        }
+    }
+`;
+
+/**
+ * Standard search by text term.
+ */
 export const SEARCH_MOVIES = gql`
     query SearchMovies($term: String!) {
         searchMovies(query: $term) {
-            id
-            name
-            overview
-            releaseDate
-            genres {
-                name
-            }
-            score
-            img: poster {
-                url: custom(size: "w500")
+            ...MovieFields
+        }
+    }
+    ${MOVIE_FIELDS}
+`;
+
+/**
+ * Search for related/similar movies based on a specific movie ID.
+ */
+export const SEARCH_SIMILAR_MOVIES = gql`
+    query SearchSimilarMovies($id: ID!) {
+        movie(id: $id) {
+            id # The parent movie ID
+            similar {
+                ...MovieFields
             }
         }
     }
+    ${MOVIE_FIELDS}
 `;
