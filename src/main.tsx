a1eb68ec6ea69d71d6239385fc3f05f592953a8b
Bereczki Sándor Máté <bsandor453@gmail.com>
2026-04-07 18:02:05 +0200

    Refactor: Add home page

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { ApolloProvider } from '@apollo/client/react';
-import { CssBaseline } from '@mui/material';
 import App from './App.tsx';
 import { apolloClient } from './api/graphql/apolloClient.ts';
-import { MUIThemeProvider } from './components/MUIThemeProvider.tsx';
 
 const rootElement = document.getElementById('root');
 
@@ -13,10 +11,7 @@ if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
       <ApolloProvider client={apolloClient}>
-        <MUIThemeProvider>
-          <CssBaseline />
-          <App />
-        </MUIThemeProvider>
+        <App />
       </ApolloProvider>
     </StrictMode>,
   );
